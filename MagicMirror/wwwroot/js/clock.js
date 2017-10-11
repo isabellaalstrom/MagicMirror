85a5c8dcf1f4b9f5ce473b16c52af9c0c8647dc0
isabellaalstrom <isabella.alstrom@gmail.com>
2017-10-11 15:55:57 +0200

    Added Javascript clock

---
+++ MagicMirror/wwwroot/js/clock.js
@@ -0,0 +1,27 @@
+﻿//function updateClock() {
+//    var currentTime = new Date();
+//    var currentHours = currentTime.getHours();
+//    var currentMinutes = currentTime.getMinutes();
+//    var currentSeconds = currentTime.getSeconds();
+//    var currentDate = currentTime.getDate();
+
+//    // Pad the minutes and seconds with leading zeros, if required
+//    currentMinutes = (currentMinutes < 10) ? ("0" + currentMinutes) : currentMinutes;
+//    currentSeconds = (currentSeconds < 10) ? ("0" + currentSeconds) : currentSeconds;
+
+
+//    // Convert an hours component of "0" to "12"
+//    currentHours = (currentHours == 0) ? 12 : currentHours;
+
+//    // Compose the string for display
+//    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " ";
+
+//    $("#time").html(currentTimeString);
+
+
+//}
+
+//$(document).ready(function () {
+//        setInterval('updateClock()', 1000);
+//    }
+//);
--- MagicMirror/wwwroot/js/site.js
+++ MagicMirror/wwwroot/js/site.js
@@ -1,1 +1,31 @@
-﻿// Write your JavaScript code.
+﻿// CLOCK
+function updateClock() {
+    var date = new Date(),
+        locale = "en-us";
+    var currentHours = date.getHours();
+    var currentMinutes = date.getMinutes();
+    var currentSeconds = date.getSeconds();
+    var currentDate = date.getDate();
+    var currentMonth = date.toLocaleString(locale, { month: "long" });
+    var currentYear = date.getFullYear();
+
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = (currentMinutes < 10) ? ("0" + currentMinutes) : currentMinutes;
+    currentSeconds = (currentSeconds < 10) ? ("0" + currentSeconds) : currentSeconds;
+
+    // Convert an hours component of "0" to "12"
+    //currentHours = (currentHours == 0) ? 12 : currentHours;
+
+    // Compose the string for display
+    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+    var currentDateString = currentDate + " " + currentMonth + " " + currentYear;
+
+    $("#time").html(currentTimeString);
+    $("#date").html(currentDateString);
+
+}
+
+$(document).ready(function () {
+        setInterval('updateClock()', 1000);
+    }
+);
