29adaf41b1b9a5712a5b1b0f6f715133364fa9cb
isabellaalstrom <isabella.alstrom@gmail.com>
2017-10-15 18:09:01 +0200

    Added SecurityComponent

---
+++ MagicMirror/Components/HassSecurityViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MagicMirror.Models;
+using MagicMirror.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace MagicMirror.Components
+{
+    public class HassSecurityViewComponent : ViewComponent
+    {
+        private readonly IHassService _hassService;
+
+        public HassSecurityViewComponent(IHassService hassService)
+        {
+            _hassService = hassService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new Security
+            {
+                Doors = await _hassService.GetAllDoorEntitiesAsync()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ MagicMirror/Models/Security.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicMirror.Models
+{
+    public class Security
+    {
+        public HassEntity Alarm { get; set; }
+        public IEnumerable<HassEntity> Doors { get; set; }
+        public IEnumerable<HassEntity> Windows { get; set; }
+        public IEnumerable<HassEntity> Pirs { get; set; }
+    }
+}
