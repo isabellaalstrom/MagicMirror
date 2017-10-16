c8cfde16b2fc5b14b5a916081105c8e053e9120f
isabellaalstrom <isabella.alstrom@gmail.com>
2017-10-16 21:20:12 +0200

    Added console application for hub and added signalr functionality for testing

---
+++ SignalR/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SignalR;
+
+public class Startup
+{
+    public void ConfigureServices(
+        IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("fiver",
+                policy => policy.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+        });
+
+        services.AddSignalR(); // &lt;-- SignalR
+    }
+
+    public void Configure(
+        IApplicationBuilder app,
+        IHostingEnvironment env)
+    {
+        app.UseCors("fiver");
+
+        app.UseSignalR(routes =>  // &lt;-- SignalR
+        {
+            routes.MapHub<ReportsPublisher> ("reportsPublisher");
+        });
+    }
+}
